5848e1c5dc052fee12f7347f362e3736d6024f57
Heidi Trambley <heidi.trambley.2025@holton-arms.edu>
2024-12-19 11:22:49 -0500

    Created club droppout page and faculty home page

--- app.rb
+++ app.rb
@@ -6,3 +6,11 @@ set :database, {adapter: "sqlite3", database: "holtonhub.sqlite3"}
 get '/' do
   "Holton Hub"
 end
+
+get '/faculty_page' do
+  erb :faculty_page
+end
+
+get '/club_droppout' do
+  erb :club_droppout
+end
