28bfb5f062fa03895e4d79a51bd9fae1ea4ab564
jriveraholton <164359247+jriveraholton@users.noreply.github.com>
2025-01-16 12:04:43 -0500

    added table to database and fixed seeds file along with property name

--- db/migrate/20241205143116_create_groups_table.rb
+++ db/migrate/20241205143116_create_groups_table.rb
@@ -1,9 +1,10 @@
+
 class CreateGroupsTable < ActiveRecord::Migration[8.0]
   def change
     create_table :groups do |t|
       t.string :name, :null => false
       t.text :description, :null => false
-      t.string :type, :null => false
+      t.string :group_type, :null => false
       t.integer :level_id, :null => false
     end
   end
