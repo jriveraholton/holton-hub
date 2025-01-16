714be64aed0c4f0364d677edf223927f10adf692
dingG25 <grace.ding.2025@holton-arms.edu>
2025-01-16 12:10:30 -0500

    added custom navbars, cleaned up event page & editing functionality

--- app.rb
+++ app.rb
@@ -46,9 +46,9 @@ class HoltonHubApp < Sinatra::Base
     if session[:access_token] != nil
 	  @active_user = User.find_by(secret: session[:access_token])
       #is no user recognized? Go to the sign_in page
-	  if(@active_user == nil)
-	    redirect '/sign_in'
-	  end
+      if(@active_user == nil)
+        redirect '/sign_in'
+      end
       @active_team_color = BwTeam.find(@active_user.team_id).team_color.downcase
     #    session[:team_color] = @active_team_color
     else
@@ -189,17 +189,17 @@ class HoltonHubApp < Sinatra::Base
     redirect '/'
   end
 
-
-
   get '/add_users' do
     erb :add_users
   end
 
-  get '/studentpage' do
-    erb :student_homepage
+  get '/student' do
+    # NEED TO ONLY AUTHORIZE IF STUDENT!!!! jfc
+    erb :student
   end
 
   get '/messages' do
+    verify_user
     erb :messages
   end
 
@@ -207,18 +207,17 @@ class HoltonHubApp < Sinatra::Base
   end 
 
   get '/new_event' do
+    verify_user
     erb :new_event
   end
 
   get '/bw_events' do
-    @events = BwEvent.all
+    @events = BwEvent.all 
+    # @events.order(:event_date).reverse
+    # puts "sorted"
     @blue_points = 0
     @white_points = 0
     verify_user
-    @access = false
-    if @active_user.is_admin
-      @access = true
-    end
     @events.each do |event| #adds up points
       @blue_points += event.blue_points
       @white_points += event.white_points
@@ -228,8 +227,7 @@ class HoltonHubApp < Sinatra::Base
   end
 
   get '/edit_event' do
-    # puts params[:id] 
-    # puts "loading"
+    verify_user
     @event = BwEvent.find_by(id: params[:id])
     erb :edit_event
   end
@@ -245,7 +243,8 @@ class HoltonHubApp < Sinatra::Base
   end
 
   post '/update_event' do
-    event = BwEvent.find_by(params[:id])
+    event = BwEvent.find_by(id: params[:id])
+    puts "Event ID" + params[:id].to_s
     name = params[:eventName]
     date = params[:date].to_datetime #calendar on the frontend
     blue_pts = params[:blue_pts]
@@ -256,7 +255,7 @@ class HoltonHubApp < Sinatra::Base
   end
 
   post '/delete_event' do
-    event = BwEvent.find_by(params[:id]) #fix here
+    event = BwEvent.find_by(id: params[:id]) 
     event.delete
     redirect '/bw_events'
   end
@@ -277,79 +276,37 @@ class HoltonHubApp < Sinatra::Base
         @all_by_groups[:facstaff].push(user)
       end
     end
-
-    get '/studentpage' do
-      erb :student_homepage
-    end
-
-    get '/messages' do
-      erb :messages
-    end
-
-    post '/messagesent' do
-    end 
-
-    get '/new_event' do
-      erb :new_event
-    end
-
-    post '/create_event' do
-      name = params[:eventName]
-      date = params[:date].to_datetime #calendar on the frontend
-      blue_pts = params[:blue_pts]
-      white_pts = params[:white_pts]
-      division = Division.find_by(name: params[:division]).id
-      puts division
-      new_event = BwEvent.create(name: name, event_date: date, blue_points: blue_pts, white_points: white_pts, division_id: division) 
-      redirect '/'
-    end
-
-    get '/manage/user_activation' do
-      verify_user
-      @all_users = User.all
-      @all_by_groups = {9 => [], 10 => [], 11 => [], 12 => [], :facstaff => []}
-
-      #figure out which users are students and put them together by grade level
-      #and put all faculty and staff together
-      @all_users.each do |user|
-        stu = Student.find_by(user_id: user.id)
-        fac = Facultystaff.find_by(user_id: user.id)
-        if stu != nil
-          @all_by_groups[stu.grade].push(user)
-        elsif fac != nil
-          @all_by_groups[:facstaff].push(user)
-        end
-      end
-
-      erb :user_activation
-    end
-
-    post '/activation' do
-      #set the given user based on name to active or inactive
-      fname = params[:fname]
-      lname = params[:lname]
-      active = params[:act]
-      user = User.find_by(firstname: fname, lastname: lname)
-      user.active = active
-      user.save
-
-      redirect '/manage/user_activation'
-    end
-
-    get '/faculty_page' do
-      erb :faculty_page
-    end
-
-    get '/club_droppout' do
-      erb :club_droppout
-    end
-
-    post '/messagesent' do
-    end 
-
-    get '/currentday' do
-      erb :day_schedule
-    end
-    ##########################################
+    erb :user_activation
   end
+
+
+  post '/activation' do
+    #set the given user based on name to active or inactive
+    fname = params[:fname]
+    lname = params[:lname]
+    active = params[:act]
+    user = User.find_by(firstname: fname, lastname: lname)
+    user.active = active
+    user.save
+
+    redirect '/manage/user_activation'
+  end
+
+  get '/faculty_page' do
+  # THIS IS NOT COMPLETE --- NEEDS TO CHECK IF USER IS FACULTY ??
+    erb :faculty_page
+  end
+
+  get '/club_droppout' do
+    erb :club_droppout
+  end
+
+  post '/messagesent' do
+  end 
+
+  get '/today' do
+    verify_user
+    erb :day_schedule
+  end
+  ##########################################
 end
